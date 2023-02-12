ff2d7656fcdf940d3e45949232ec89b6999dba7a
NAVEEN <naveenchowdaryvemuri@gmail.com>
2023-02-12 17:19:39 +0530

    Pushing addition,subtraction,Multiplication,divison and String examples

---
+++ src/StringDatatype.java
@@ -0,0 +1,8 @@
+public class StringDatatype {
+    public static void main(String[] args)
+    {
+        String name="srilatha";
+        String name1="xyz";
+        System.out.println(name);
+    }
+}
