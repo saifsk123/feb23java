123bd548549f47252483bc40b19874abe2f65952
NAVEEN <naveenchowdaryvemuri@gmail.com>
2023-02-17 10:26:30 +0530

    Adding Access Modifier Methods

---
+++ src/PublicAccesModifier.java
@@ -0,0 +1,24 @@
+public class PublicAccesModifier
+{
+    //****Basic Method declaration without parameters:AccessModifier returntype methodname();***
+    //***() - method brackets
+    public void publicMethod()  //method declaration
+    {
+        //Method body
+        String name="Naveen";
+        System.out.println(name);
+    }
+    public void output()
+    {
+        System.out.println("saif");
+    }
+
+    public static void main(String[] args)
+    {
+       //***object creation syntax---- Classname obj=new Classname();-----***
+        PublicAccesModifier obj=new  PublicAccesModifier();
+        obj.publicMethod();
+        obj.output();
+    }
+
+}
---
+++ src/ReturnTypeMethods.java
@@ -0,0 +1,74 @@
+public class ReturnTypeMethods
+{
+    //**without returning value***//
+    public void add()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+    /*
+    public void add30and40()
+    {
+        int a=30;
+        int b=40;
+        int c=a+b;
+        System.out.println(c);
+    }
+*/
+    //returnning a value
+    public int addition()
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        return c;
+
+    }
+
+    //** method with parameters***//
+
+    public int paramAddition(int a,int b)
+    {
+        int c=a+b;
+        return  c;
+    }
+
+    public void multiply()
+    {
+        int a=10;
+        int b=20;
+        int c=a*b;
+        System.out.println(c);
+    }
+
+    public  void sub()
+    {
+        int a=20;
+        int b=10;
+        int c=a-b;
+        System.out.println(c);
+    }
+
+    public static void main(String[] args)
+    {
+        ReturnTypeMethods obj=new ReturnTypeMethods();
+        //return type with void
+        obj.add();
+        //***Variable saving syntax:Datatype variable=value;***/
+        int d=obj.addition();
+        int e=d*20;
+        System.out.println(e);
+
+        //** calling parameterization method***//
+
+        int g=obj.paramAddition(30,40);
+        System.out.println(g);
+        int f=obj.paramAddition(70,30);
+        System.out.println(f);
+
+    }
+
+}
