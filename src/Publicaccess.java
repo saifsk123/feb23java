82ebde19b8973cc2191648aca17107c7e9a358af
dev <dev@gmail.com>
2023-02-17 19:02:22 +0530

    Parameters by Nikhil

---
+++ src/Publicaccess.java
@@ -0,0 +1,13 @@
+public class Publicaccess
+{
+    public void main()
+    {
+        String name="nikhil";
+        System.out.println(name);
+    }
+
+    public static void main(String[] args) {
+        Publicaccess obj=new Publicaccess();
+        obj.main();
+    }
+}
