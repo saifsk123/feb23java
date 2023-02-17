8240553f63723570a98f54f9e2e7babc0a76ba9e
dev <dev@gmail.com>
2023-02-17 18:54:32 +0530

    Parameters by Nikhil

---
+++ src/Parameters.java
@@ -0,0 +1,15 @@
+public class Parameters
+{
+    public int add(int a,int b)
+    {
+        int c=a+b;
+        return c;
+    }
+
+    public static void main(String[] args)
+    {
+        Parameters obj=new Parameters();
+        int c=obj.add(12,48);
+        System.out.println(c);
+    }
+}
