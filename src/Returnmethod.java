806a2472aa7667dae311ff318893c61ac730a1a5
dev <dev@gmail.com>
2023-02-17 19:11:37 +0530

    Return by Nikhil

---
+++ src/Returnmethod.java
@@ -0,0 +1,15 @@
+public class Returnmethod
+{
+    public int multiply()
+    {
+        int a=356;
+        int b=765;
+        return (b-a);
+    }
+    public static void main(String[] args)
+    {
+        Returnmethod obj=new Returnmethod();
+        int c=obj.multiply();
+        System.out.println(c);
+    }
+}
