3c425f0d34b623ed5b3e5d924fb3fb1605c65e11
NAVEEN <naveenchowdaryvemuri@gmail.com>
2023-02-23 10:08:03 +0530

    Ravi Practice

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,10 +1,10 @@
 public class LoginPage {
     public void verifyloginbutton()
     {
-        String s1="naveen1";
+        String s1="RaviShankar";
         String s2="naveen3";
         String s3="naveen4";
-        String s4="naveen5;
+        String s4="naveen5";
 
     }
 }
