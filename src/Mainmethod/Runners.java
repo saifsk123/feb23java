7b60b53cb1ab192033229238e373ecfc596c4c45
dev <dev@gmail.com>
2023-02-26 22:32:54 +0530

    adding session

---
+++ src/Mainmethod/Runners.java
@@ -0,0 +1,14 @@
+package Mainmethod;
+
+import Variables2.Nonstatics;
+import Variables2.Statics;
+
+public class Runners {
+    public static void main(String[] args) {
+        Nonstatics obj=new Nonstatics();
+        String s=obj.name;
+        System.out.println(s);
+        String f= Statics.name;
+        System.out.println(f);
+    }
+}
