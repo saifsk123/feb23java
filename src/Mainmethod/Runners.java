36207b9d9523a03f3c052b218081b7812f0fdf2c
NAVEEN <naveenchowdaryvemuri@gmail.com>
2023-03-06 11:40:58 +0530

    while and dowhile statemnets

--- src/Mainmethod/Runners.java
+++ src/Mainmethod/Runners.java
@@ -5,19 +5,9 @@ import Variables2.Statics;
 
 public class Runners {
     public static void main(String[] args) {
-        Nonstatics obj=new Nonstatics();
-        String s=obj.name;
+        Nonstatics obj = new Nonstatics();
+        String s = obj.name;
         System.out.println(s);
-        String f= Statics.name;
-import Variables2.Nonstatics2;
-import Variables2.Statics2;
-
-public class Runners {
-    public static void main(String[] args) {
-        Nonstatics2 obj=new Nonstatics2();
-        int e=obj.age;
-        System.out.println(e);
-        int f= Statics2.age;
-        System.out.println(f);
+        String f = Statics.name;
     }
 }
