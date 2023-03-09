1c0cfddaae3e6d7d1333f5ee0ead03254f228456
dev <dev@gmail.com>
2023-03-09 22:11:38 +0530

    for loops

---
+++ src/Forloops/For.java
@@ -0,0 +1,12 @@
+package Forloops;
+
+public class For {
+    public static void main(String[] args) {
+        for (int r=1;r<=6;r++){
+            for (int s=1;s<=r;s++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Forloops/Reverse.java
@@ -0,0 +1,12 @@
+package Forloops;
+
+public class Reverse {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            for (int t=i;t<=5;t++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
